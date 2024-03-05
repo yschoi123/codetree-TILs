0434560bed366e92d97f2ceeaa34b0774547884f
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-05 17:49:27 +0900

    Title: 별 n개 출력하기, Time: 192ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        // n 입력 받기 
+        int n = sc.nextInt();
+
+        //  n개의 줄에 걸쳐 "*" 하나씩 출력
+        while(n-- > 0){
+            System.out.println("*");
+        }
+    }
+}
