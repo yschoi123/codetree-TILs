926836fdbc2ffc743f006ae4c91dda2ca83cbfe1
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-05 17:02:52 +0900

    Title: 4번의 크기 비교, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        // 입력 받기
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+
+        // a > b 비교
+        if( a > b ) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+
+        // a > c 비교
+        if( a > c ) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+
+        // a > d 비교
+        if( a > d ) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+
+        // a > e 비교
+        if( a > e ) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    
+    }
+}
