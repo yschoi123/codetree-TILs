be344de5967a95a6a5533e6b9df9477a7caf24c5
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-06 00:47:53 +0900

    Title: 조건에 따른 연산, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        while(n  < 1000) {
+            if(n%2 == 0){
+                n = 3*n + 1;
+            } else {
+                n = 2*n + 1;
+            }
+
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
