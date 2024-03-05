a30db95dd0af70417cb962b529c86bab9922ad8d
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-06 03:08:32 +0900

    Title: 별표 출력하기 13, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            if(i%2==0) {
+                for(int j=0; j<n-i/2; j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for(int j=0; j<i/2+1; j++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+
+        for(int i=n-1; i>=0; i--){
+            if(i%2==0) {
+                for(int j=0; j<n-i/2; j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for(int j=0; j<i/2+1; j++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
