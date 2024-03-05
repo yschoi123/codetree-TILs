6f3248dd6389fe6c2184df3354061ea96d1e4320
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-05 18:27:47 +0900

    Title: 1까지 나누기, Time: 120ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240305/1까지 나누기/divide-by-1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+        for(int i = 1; ; i++){
+            n /= i;
+            count += 1;
+            if(n < 1){
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
