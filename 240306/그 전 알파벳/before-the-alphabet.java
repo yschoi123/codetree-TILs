20dd49785d916e91fdd93a1fc5a0fa785119b41d
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-06 22:14:57 +0900

    Title: 그 전 알파벳, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+
+        if(c=='a'){
+            System.out.print('z');
+        } else {
+            System.out.print((char)(c-1));
+        }
+    }
+}
