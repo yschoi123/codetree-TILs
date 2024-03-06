3e0ab06eb30162e6f42aa94033594ecb32be6736
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-06 18:01:36 +0900

    Title: 짝수들을 거꾸로 출력, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/짝수들을 거꾸로 출력/print-even-numbers-upside-down.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] values = new int[n];
+
+        for(int i=0; i<n; i++){
+            values[i] = sc.nextInt();
+        }
+
+        for(int i=n-1; i>=0; i--){
+            if(values[i]%2==0){
+                System.out.print(values[i]+" ");
+            }
+        }
+    }
+}
