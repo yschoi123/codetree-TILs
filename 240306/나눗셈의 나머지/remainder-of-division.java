c0d39178c2dec7612526b82db3a0eb7a6a68b9b3
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-06 18:19:11 +0900

    Title: 나눗셈의 나머지, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/나눗셈의 나머지/remainder-of-division.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] counts = new int[10]; 
+        while(a>1){
+            int remain = a%b;
+            a /= b;
+
+            counts[remain] += 1;
+        }
+
+        int result = 0;
+        for(int i=0; i<10; i++){
+            result += counts[i] * counts[i];
+        }
+        System.out.print(result);
+    }
+}
