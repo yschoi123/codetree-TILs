dc72f0d22cc01ed23e1be1e6e59b823d90b744f5
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-06 17:49:27 +0900

    Title: 알파벳 출력하기 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        char alpa = 'A';
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("  ");
+            }
+            for(int j=0; j<n-i; j++){
+                System.out.print(alpa+" ");
+                alpa += 1;
+            }
+            System.out.println();
+        }
+    }
+}
