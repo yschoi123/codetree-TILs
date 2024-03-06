72671c43a65d9d946972b260e4a3b7525f2e4811
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-03-06 19:58:29 +0900

    Title: 격자로 사각형 만들기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+
+    
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i][j-1] + arr[i-1][j] + arr[i-1][j-1];
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
